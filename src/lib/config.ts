0ce593302458fad623fb91fe142049ae752bc23b
Espryra <espryra@gmail.com>
2025-08-04 16:03:56 -0600

    First commit

---
+++ src/lib/config.ts
@@ -0,0 +1,19 @@
+const Config = {
+  debug: true,
+
+  // General
+  sounds: {
+    info: "note.hat",
+    success: "note.pling",
+    warning: "note.harp",
+    error: "note.bass",
+  },
+  colors: {
+    info: "",
+    success: "§a",
+    warning: "§e",
+    error: "§c",
+  },
+};
+
+export default Config;
