36651b8328664b36d8ab86f11af11502594c6359
Espryra <espryra@gmail.com>
2026-04-03 08:36:42 -0600

    Removed debug stick lol

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,4 @@
-import { world } from "@minecraft/server";
 import "./protos/loader";
 import CommandHandler from "./utils/commandHandler/handler";
-import ActionForm from "./utils/form/action";
 
 CommandHandler.Init();
-
-world.afterEvents.itemUse.subscribe(async (event) => {
-  const { source: player, itemStack: item } = event;
-
-  if (item.typeId !== "minecraft:stick") {
-    return;
-  }
-
-  new ActionForm()
-    .SetTitle("Testing")
-    .SetBody([`Hello, ${player.name}!\n`, "What would you like to do?\n"])
-    .AddButton("Testing", "Click to Open", "textures/items/emerald")
-    .Show(player);
-});
